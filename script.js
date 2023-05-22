fd9f0b7dba8e07075a64ef39369f933256980a41
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-22 19:30:02 +0300

    Modern JavaScript Development

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // addToCart('bread', 5)
 // console.log(price, tq)
 
+/*
+import shoppingCart from "./shoppingCart";
+
 console.log('Importing module')
     // console.log(shippingCost)
 
@@ -41,3 +44,33 @@ console.log(lastPost)
 
 const lastPost2 = await getLastPost()
 console.log(lastPost2)
+*/
+
+//* The Module pattern
+const shoppingCart2 = (function() {
+    const cart = [];
+    const shippingCost = 10
+    const totalPrice = 237
+    const totalQuantity = 23
+
+    const addToCart = function(product, quantity) {
+        const cart = []; // Initialize the `cart` variable as an empty array
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart (shipping cost ${shippingCost})`);
+    };
+
+    const orderStock = function(product, quantity) {
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity
+    }
+})()
+
+shoppingCart2.addToCart('apple', 4)
+shoppingCart2.addToCart('pizza', 2)
+console.log(shoppingCart2)
